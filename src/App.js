de25225a65586bc8ba00ef56ce7caf93e83003b8
Kjohnson870 <karjohnson08@yahoo.com>
2023-07-17 17:43:32 -0500

    router

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Home from './components/Home'
+import Contact from './components/Contact'
+import Menu from './components/Menu'
+import Navbar from './components/Navbar'
+import './App.css'
+import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
+
+function App() {
+  return (
+    <Router>
+      <Navbar/>
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path='/contact' element={<Contact/>}/>
+        <Route path='/menu' element={<Menu/>}/>
+        </Routes>
+      </Router>
+      
+  );
+}
+
+export default App;
